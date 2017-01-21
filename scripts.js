9ffa379e4c912c0a7f021efc8fa6a784d85740c9
eahn2016 <eugeneahn@lawn-143-215-95-94.lawn.gatech.edu>
2017-01-21 16:18:06 -0500

    I changed an ID

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 function doSomething() {
     var songName = document.getElementById('song').value;
     var artistName = document.getElementById('artist').value;
-    document.getElementById('songTitle').innerHTML = songName + " - " + artistName;
+    document.getElementById('songTitle').innerHTML = songName;
+    document.getElementById('artistName').innerHTML = artistName;
 
     document.getElementsByClassName('secondPage')[0].style.display = "inline";
     document.getElementsByClassName('firstPage')[0].style.display = "none";
