4d5195ae86b5e08e33f82a3058568d96f4053d18
Kun Chen <kc75997@uga.edu>
2017-01-21 20:31:11 -0500

    updated UI rendering

--- setup.js
+++ setup.js
@@ -6,12 +6,24 @@ function setup() {
     }
 
     chrome.runtime.sendMessage(message, function(response) {
-        console.log(response.result)
+        //console.log(response.result)
+        var array = top25(response.result)
+
+        for (i = 0; i < array.length; i++) {
+            array[i] = array[i].trim();
+        }
+        //console.log(array)
+        updateTable(array)
     })
 }
 
-function updateTable() {
-
+function updateTable(array) {
+    var arrayOfDest = $(document).find("tbody").find("td:odd")
+    //var arrayOfDest = $(document).find("tbody").find("tr")
+    for (i = 0; i < arrayOfDest.length; i++) {
+        $(arrayOfDest[i]).text(array[i])
+    }
+    console.log(array.length - arrayOfDest.length)
 }
 
 $(document).ready(setup())
