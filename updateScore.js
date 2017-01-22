820221281193fe8d9ec9dd087b60f68cbaabcc06
Kun Chen <kc75997@uga.edu>
2017-01-21 20:56:26 -0500

    added coloring for score

--- updateScore.js
+++ updateScore.js
@@ -1,3 +1,13 @@
 function updateScore(score) {
     $(document).find(".number").text(score)
+
+    if (score <= 33) {
+        $(document).find(".number").css('color', 'red')
+    } else {
+        if (score <= 67) {
+            $(document).find(".number").css('color', 'white')
+        } else {
+            $(document).find(".number").css('color', 'blue')
+        }
+    }
 }
